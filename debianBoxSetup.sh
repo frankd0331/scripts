e22201a7cfba57e492f60dfa26e69bcd1cec5d25
Frank Dudek <dudek.f.p@gmail.com>
2013-08-25 15:59:34 -0500

    updated debian script

--- debianBoxSetup.sh
+++ debianBoxSetup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# setup for a Ubuntu 12.04 LTS box
-# should work on any debian based distro
+# setup for a Debian Stable box
 # I'm just learning this stuff so if you're reading this
 # it's probably best to look elsewhere
 
@@ -9,12 +8,9 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-# installing git, vim, emacs, terminator, guake, curl
-sudo apt-get install -y git
+# installing vim, emacs, terminator, guake, curl
 sudo apt-get install -y vim
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+sudo apt-get install -y emacs 
 sudo apt-get install -y terminator
 sudo apt-get install -y guake
 sudo apt-get install -y curl
@@ -32,29 +28,10 @@ mkdir terminator
 cd terminator
 ln -sb ~/dotfiles/config.terminator config
 
-# emacs with ensime
+# make a home bin dir
 cd $HOME
 mkdir bin
-cd bin
-wget https://www.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz
-tar -xvf ensime_2.10.0-0.9.8.9.tar.gz
-cd $HOME
-ln -sb dotfiles/.emacs
-# not sure how to automate it but you'll have to:
-# M-x package-install RET evil RET
-# in emacs to setup evil-mode
-
-# vim
-ln -sb dotfiles/.vimrc
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-cd .vim/bundle
-git clone git://github.com/vim-scripts/tComment.git
-git clone git://github.com/Lokaltog/vim-easymotion.git
-git clone git://github.com/scrooloose/nerdtree.git
-git clone git://github.com/derekwyatt/vim-scala.git
-git clone git://github.com/dahu/Insertlessly.git
-git clone git://github.com/altercation/vim-colors-solarized.git
+ln -sb dotfiles/emacs23 .emacs.d
 
 # disabling <Ctrl-s> in terminal
 cd $HOME
@@ -72,23 +49,10 @@ echo "export TERM=xterm-256color" >> .bashrc
 cd $HOME
 mkdir projects
 
-# install Scala
+# install jdk7
 sudo apt-get install -y openjdk-7-jdk
-cd bin
-wget http://www.scala-lang.org/files/archive/scala-2.10.2.tgz
-tar -xvf scala-2.10.2.tgz
-cd $HOME
-echo "" >> .bashrc
-echo "# adding Scala to path" >> .bashrc
-echo "export PATH=$PATH:/home/frankd/bin/scala-2.10.2/bin" >> .bashrc
 
-cd bin
-wget http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch//0.12.4/sbt-launch.jar
-cd $HOME
-cd scripts
-cp sbt ~/bin
-cd $HOME
-chmod u+x ~/bin/sbt
+# add home bin to path
 echo "" >> .bashrc
 echo "# I thought my home bin was already in path, but just in case" >> .bashrc
 echo "export PATH=$PATH:/home/frankd/bin" >> .bashrc
