cd1d41ce8946ae61a7c3be2eab17e5d07fb041bd
Frank Dudek <dudek.f.p@gmail.com>
2013-12-30 21:36:54 -0600

    some fixes

--- ubuntuBoxSetup.sh
+++ ubuntuBoxSetup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # setup for an Ubuntu Distro
+# specifically xubuntu
 #
 # You will need to chmod u+x debianBoxSetup.sh in order to run it
 
@@ -9,25 +10,10 @@ sudo apt-get -y upgrade
 
 echo "*****installing vim, emacs, terminator, curl*****"
 sudo apt-get install -y git
-sudo apt-get install -y vim
 sudo apt-get install -y terminator
 sudo apt-get install -y curl
-sudo apt-get install -y racket
-
-# replace this with ubuntu appropriate emacs
-# and any other needed ppa's
-echo "*****myppa.list*****"
-cd $HOME
-mkdir tmp
-touch tmp/myppa.list
-#echo "deb http://emacs.naquadah.org/ stable/" >> tmp/myppa.list
-sudo cp tmp/myppa.list /etc/apt/sources.list.d/
-rm tmp/myppa.list
-
-echo "*****Installing Emacs 24*****"
-#wget -q -O - http://emacs.naquadah.org/key.gpg | sudo apt-key add -
-#sudo apt-get update
-#sudo apt-get install -y emacs-snapshot-nox
+sudo apt-get install -y gnome-do
+sudo apt-get install -y emacs24-nox
 
 echo "*****Setting up my dotfiles*****"
 
@@ -70,7 +56,6 @@ echo "# I thought my home bin was already in path, but just in case" >> .bashrc
 echo "export PATH=$PATH:/home/frankd/bin" >> .bashrc
 
 echo "*****install node*****"
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 echo "*****mongoDB*****"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
@@ -78,18 +63,11 @@ echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' |
 sudo apt-get update
 sudo apt-get install -y mongodb-10gen
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
-
 echo "*****Reload bash*****"
 cd $HOME
 source .bashrc
 
 # ToDo:
 # Test
+# node
+# make an nvidia script
